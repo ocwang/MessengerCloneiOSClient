a8a5a3b8bc0f9194bdec7b42c10637d88c158496
Chase Wang <chase@ocwang.com>
2016-07-23 23:18:51 -0700

    Added text input view

---
+++ MessengerClone/MCTextInputView.swift
@@ -0,0 +1,13 @@
+//
+//  MCTextInputView.swift
+//  MessengerClone
+//
+//  Created by Chase Wang on 7/23/16.
+//  Copyright © 2016 ocwang. All rights reserved.
+//
+
+import UIKit
+
+class MCTextInputView: UIView {
+    
+}
